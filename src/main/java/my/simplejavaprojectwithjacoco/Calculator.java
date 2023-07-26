1a4615f72a825e7eab36cf276bfed42cd3480c04
ramsabari97 <139346554+ramsabari97@users.noreply.github.com>
2023-07-26 16:37:43 +0530

    Update Calculator.java

--- src/main/java/my/simplejavaprojectwithjacoco/Calculator.java
+++ src/main/java/my/simplejavaprojectwithjacoco/Calculator.java
@@ -7,10 +7,10 @@ public class Calculator {
 		return a+b;
 	}
 
-	// public int minus(int a, int b)
-	// {
-	// 	return a-b;
-	// }
+	public int minus(int a, int b)
+	{
+		return a-b;
+	}
 	
 	public int divide(int a, int b)
 	{
